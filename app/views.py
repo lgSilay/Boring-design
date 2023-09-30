ef7ad84a6ff7fd063d9d5167ceaea17176b7a424
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-09-30 18:16:10 +0300

    Fix naming in project_detail.html and add type annotation for view func

--- app/views.py
+++ app/views.py
@@ -1,18 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
 from app.models import Style, Project
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
 
 
-def contacts_view(request):
+def contacts_view(request: HttpRequest) -> HttpResponse:
     return render(request, "contacts.html")
 
 
-def projects_list_view(request):
+def projects_list_view(request: HttpRequest) -> HttpResponse:
     context = {
         "styles": Style.objects.all(),
         "projects": Project.objects.all(),
