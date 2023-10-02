9de82e8916ad5eee4c7696645e41c9321f2b2d58
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-10-02 12:08:31 +0300

    Rewrite View func for project_detail.html, add comments section

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
 
-from app.models import Style, Project
+from app.forms import CommentForm
+from app.models import Style, Project, Commentary
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -25,6 +27,38 @@ def projects_list_view(request: HttpRequest) -> HttpResponse:
     )
 
 
-class ProjectDetailView(generic.DetailView):
-    model = Project
-    template_name = "project_detail.html"
+def projects_filtered_list_view(request: HttpRequest, pk: int) -> HttpResponse:
+    context = {
+        "styles": Style.objects.all(),
+        "projects": Project.objects.filter(style__id=pk),
+    }
+    return render(
+        request,
+        "projects_list.html",
+        context=context
+    )
+
+
+def project_detail_view(request: HttpRequest, pk) -> HttpResponse:
+    project = Project.objects.get(id=pk)
+
+    if request.method == "GET":
+        form = CommentForm(initial={"user": request.user})
+        context = {"form": form, "project": project}
+        return render(request, "project_detail.html", context=context)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if request.user.is_anonymous:
+            form.add_error("content", "Please log in to add a comment")
+        elif form.is_valid():
+            Commentary.objects.create(
+                user=request.user, project=project, **form.cleaned_data
+            )
+            return HttpResponseRedirect(
+                reverse("app:projects_detail", kwargs={"pk": pk})
+            )
+
+        context = {"form": form, "project": project}
+
+        return render(request, "project_detail.html", context=context)
